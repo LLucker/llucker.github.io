8e37ec55d8f8e5e4f86ebb064b0dde7fa58e6183
Luan Sena <luansena@live.com>
2016-10-28 18:04:55 -0200

    casting

---
+++ casting/assets/js/preloader.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(window).load(function(){
+        //$('#preloader').fadeOut(1500);
+        alert('teste');
+        document.getElementById('oculto').id = '';
+        //document.getElementById('preloader').id = 'oculto';
+    });
+});
